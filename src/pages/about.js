50cace19fe49dee06207d88a7429658cff5559bc
Renato Babata <babatarenato@gmail.com>
2020-07-19 10:10:58 -0300

    Coffee - 2:40

---
+++ src/pages/about.js
@@ -0,0 +1,35 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import BackgroundSection from "../components/Globals/BackgroundSection"
+import Info from "../components/Home/Info"
+
+const AboutPage = ({data}) => (
+  <Layout>
+    <SEO title="Home" />
+    <BackgroundSection img={data.img.childImageSharp.fluid} 
+    title="About us" styleClass="default-background" styleClass="about-background"/>
+    <Info/>
+    <Link to="/about">
+      <button className="btn text-uppercase btn-yellow"> About</button>
+    </Link>
+  </Layout>
+)
+
+export const query = graphql`
+  {
+    img:file(relativePath:{eq:"about-background.jpeg"}){
+      childImageSharp{
+        fluid{
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`;
+
+
+
+export default AboutPage

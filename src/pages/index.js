4a242c7a7e56193dd91abcaba90bd8a131cb52c3
Renato Babata <babatarenato@gmail.com>
2020-07-20 15:00:57 -0300

    final commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,7 @@ const IndexPage = ({data}) => (
 
 export const query = graphql`
   {
-    img:file(relativePath:{eq:"default-background.jpeg"}){
+    img:file(relativePath:{eq:"default-background.jpg"}){
       childImageSharp{
         fluid{
           ...GatsbyImageSharpFluid_tracedSVG
